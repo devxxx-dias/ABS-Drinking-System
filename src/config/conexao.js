623bf09375929b365835829143a649cb63c2955f
Maurício Dias <devxxx.mauricio@gmail.com>
2024-02-09 22:04:27 -0300

    ajuste do arquivo conexao

--- src/config/conexao.js
+++ src/config/conexao.js
@@ -1,11 +1,13 @@
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
+        host: process.env.DEPLOY_HOST,
+        port: process.env.DEPLOY_PORT,
+        user: process.env.DEPLOY_USER,
+        password: process.env.DEPLOY_PASSWORD,
+        database: process.env.DEPLOY_DATABASE,
+        uri: process.env.DEPLOY_URI,
+        ssl: { rejectUnauthorized: false },
     }
 })
 
