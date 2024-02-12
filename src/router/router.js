c8037d9db823059e298899825088bf2ef09c2be0
Maurício Dias <devxxx.mauricio@gmail.com>
2024-02-12 18:18:44 -0300

    teste router

--- src/router/router.js
+++ src/router/router.js
@@ -1,8 +1,8 @@
 const { Router } = require('express');
 const validarJoi_Body = require('../middleware/validarJoi');
 const checar_Pedido = require('../validation/schemaPedido');
-const realizarConsulta = require('../controllers/ConsultaController');
-const lancarPedido = require('../controllers/pedidoController');
+const realizarConsulta = require('../controllers/consultaController.js');
+const lancarPedido = require('../controllers/PedidoController.js');
 
 const router = Router();
 
